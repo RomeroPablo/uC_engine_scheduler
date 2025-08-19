4b3e229f8db225ec9b77f55660937806529618a6
RomeroPablo <romeropablojr03@gmail.com>
2025-08-19 12:18:44 -0500

    init

---
+++ main.c
@@ -0,0 +1,31 @@
+// simulator for AMDs micro engine scheduler
+
+/* requirements: 
+   fair & efficient scheduling for gpu workloads
+   mulitple priority levels
+   same level of priority => equal amount execution time
+   user focused work (compositor) should receive larger amount of time, but not starve the GPU
+   real-time work should run immediately and can infinitely starve work in lower priority levels
+   low priority works when other levels are idle
+
+   tRealTime
+   tFocus
+   tNormal
+   tLow
+
+*/
+
+#include "stdio.h"
+
+enum priority {
+    realTime,
+    focus,
+    normal,
+    low
+} typedef priority_t;
+
+int main(){
+    priority_t x = realTime;
+    printf("[+] prio: %i \n", x);
+    return 0;
+}
