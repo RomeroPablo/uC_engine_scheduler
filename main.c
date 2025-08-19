542a7dc27b41c078a63172c8dd416d06b47f13eb
RomeroPablo <romeropablojr03@gmail.com>
2025-08-19 12:21:08 -0500

    readme

--- main.c
+++ main.c
@@ -1,18 +1,12 @@
 // simulator for AMDs micro engine scheduler
 
-/* requirements: 
+/*requirements: 
    fair & efficient scheduling for gpu workloads
    mulitple priority levels
    same level of priority => equal amount execution time
    user focused work (compositor) should receive larger amount of time, but not starve the GPU
    real-time work should run immediately and can infinitely starve work in lower priority levels
    low priority works when other levels are idle
-
-   tRealTime
-   tFocus
-   tNormal
-   tLow
-
 */
 
 #include "stdio.h"
