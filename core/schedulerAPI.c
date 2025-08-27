e89b27b89a290c43706b26aa78cdb9b9537b8b3c
RomeroPablo <romeropablojr03@gmail.com>
2025-08-27 16:49:42 -0500

    readme?

---
+++ core/schedulerAPI.c
@@ -0,0 +1,7 @@
+/* Scheduler APIs
+
+   The commands sent by the driver to inform scheduler of the events such as:
+   queue creation, destruction, suspension, or any changes to its priority.
+   Each API is described later under APIs section
+
+*/
